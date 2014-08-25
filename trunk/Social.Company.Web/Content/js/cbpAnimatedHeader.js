8334004191b1fa0d2ccf53892c7d85c4f6b1e1ba
davidgrupp <justinjarczyk@gmail.com>
2014-08-25 15:00:40 -0500

    Modified the transition of Branding Logo, Consolidated logo when in navbar mode

--- trunk/Social.Company.Web/Content/js/cbpAnimatedHeader.js
+++ trunk/Social.Company.Web/Content/js/cbpAnimatedHeader.js
@@ -13,6 +13,9 @@ var cbpAnimatedHeader = (function() {
 	var docElem = document.documentElement,
 		header = document.querySelector('.navbar-default'),
         navItems = document.querySelector('.navItemSelector'),
+        headerLogo = document.querySelector('.top-logo-fixed'),
+        headerShort = document.querySelector('.top-logo-short'),
+
 		didScroll = false,
 		changeHeaderOn = 300;
 
@@ -30,11 +33,14 @@ var cbpAnimatedHeader = (function() {
 		if ( sy >= changeHeaderOn ) {
 		    classie.add(header, 'navbar-shrink');
 		    classie.remove(navItems, 'customHide');
+		    classie.add(headerLogo, 'hide');
+		    classie.remove(headerShort, 'hide');
 		}
 		else {
 		    classie.remove(header, 'navbar-shrink');
 		    classie.add(navItems, 'customHide');
-
+		    classie.remove(headerLogo, 'hide');
+		    classie.add(headerShort, 'hide');
 		}
 		didScroll = false;
 	}
